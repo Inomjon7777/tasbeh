ebef2dc470ef29cd0c403d9c80aae54ec3de72d7
Jovliyev Inomjon <91968320+Inomjon7777@users.noreply.github.com>
2022-05-07 11:42:33 +0500

    first commit

---
+++ tasbeh.js
@@ -0,0 +1,17 @@
+let myShow = document.querySelector('.show');
+let myAdd = document.querySelector('.add');
+let myLight = document.querySelector('.led');
+let myRemove = document.querySelector('.remove');
+
+myLight.addEventListener("click", function () {
+    myShow.style.backgroundColor = "#00ff77";
+});
+
+myAdd.addEventListener("click", function () {
+    let adder = document.querySelector('.adder')
+    adder.value = parseInt(adder.value) + 1;
+});
+
+myRemove.addEventListener("click", function () {
+    myShow.innerHTML = 0;
+});
